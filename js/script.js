1e44f731f634d791594f289551f20afb5f6f5e77
MayaSelena773 <maya.bono721@gmail.com>
2022-06-17 16:35:07 +0200

    beginourteam

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+// Aggiungere un file js in cui definire un array di oggetti che rappresentano i membri del team. 
+// Ogni membro ha le informazioni necessarie per stampare la relativa card: Nome, Ruolo e Foto.
+// Una volta definita la struttura dati, prendendo come riferimento la card di esempio presente nell'html, 
+// stampare dinamicamente una card per ogni membro del team.
+
+//PASSAGGI
+//Creo l' array di oggetti con nome, ruolo e foto di ogni membro del team
